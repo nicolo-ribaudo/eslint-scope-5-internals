ceccb38a6beb7f444a936cc0873f1ace99104422
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-09-05 16:54:02 +0200

    Fix `Definition` export

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     ...require("eslint-scope"),
-    Definition: require("eslint-scope/lib/definition"),
+    Definition: require("eslint-scope/lib/definition").Definition,
     PatternVisitor: require("eslint-scope/lib/pattern-visitor"),
     Referencer: require("eslint-scope/lib/referencer"),
 };
